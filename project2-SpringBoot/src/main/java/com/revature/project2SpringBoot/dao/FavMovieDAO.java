f56c5dba971d034fb0e013127ab90a1d21d47ec4
Carl Pacquing <cjpacquing@gmail.com>
2019-07-13 12:49:22 -0400

    Added db folder and started FavMovieDao

--- project2-SpringBoot/src/main/java/com/revature/project2SpringBoot/dao/FavMovieDAO.java
+++ project2-SpringBoot/src/main/java/com/revature/project2SpringBoot/dao/FavMovieDAO.java
@@ -1,5 +1,14 @@
 package com.revature.project2SpringBoot.dao;
 
-public interface FavMovieDAO {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.revature.project2SpringBoot.model.Movie;
+
+public interface FavMovieDAO extends JpaRepository<Movie,Integer>{
+	//findAll
+	//saveFavorite
+	void saveFavorite(Movie m);
+	List<Movie> findAllFavorites();
 }
